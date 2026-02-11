958581b989e4eb76600996cb9992ff60db4fb2f5
Aliou <aladdinelemin@gmail.com>
2026-02-11 03:35:07 +0000

    dernier commit

--- secure_doc/settings.py
+++ secure_doc/settings.py
@@ -218,3 +218,32 @@ CORS_ALLOW_HEADERS = [
     "origin",
     "x-csrftoken",
 ]
+
+
+
+# Pour Render
+import os
+from decouple import config
+
+if not DEBUG:
+    # Static files
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
+    # Database (Render fournit DATABASE_URL)
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+    
+    # MinIO/Backblaze B2
+    AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL')
+    AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL.split('//')[1]}"
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_DEFAULT_ACL = 'private'
+    AWS_S3_FILE_OVERWRITE = False
+    
+    # Utiliser S3 pour les fichiers uploadés
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
