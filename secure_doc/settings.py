9c178850f8dce46b8d85cad052f169d401c266d6
Aladdin24 <aladdinelemin@gmail.com>
2026-02-15 02:54:54 +0000

    Description de la modification

--- secure_doc/settings.py
+++ secure_doc/settings.py
@@ -255,9 +255,6 @@ if USE_S3:
 
 # Static files (obligatoire pour Render)
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
